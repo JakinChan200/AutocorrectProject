5734f703c94419f21628ee4ff0290c53bac43368
Jakin Chan <JakinChan200@gmail.com>
2025-01-27 23:30:02 -0800

    Implemented attempt 2

--- Attempt1.hpp
+++ Attempt1.hpp
@@ -1,11 +1,65 @@
 //2019
 //Triple for loop checking for every letter in string a, try every 
 
-#include<bits/stdc++.h>
+#ifndef ATTEMPT_1
+#define ATTEMPT_1
+
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
+int minDistance1(string word1, string word2) {
+	int longestStreak = 0;
+	int streak = 0;
+	for(int j = 0; j < word1.length(); j++){
+		for(int k = 0; k < word2.length(); k++){
+			if(((k < word2.length()) && (j < word1.length())) && ((tolower(word2[k]) == tolower(word1[j]) || ((word2[k] == ' ') && (word1[j] == ' '))))){
+				while((k < word2.length() && j < word1.length()) && ((tolower(word2[k]) == tolower(word1[j]) || ((word2[k] == ' ') && (word1[j] == ' '))))){
+					streak++;
+					k++;
+					j++;
+				}
+				if(streak > longestStreak){
+					longestStreak = streak;
+				}
+			}else{
+				streak = 0;
+			}
+		}
+	}
+	return longestStreak;
+}
+
 vector<string> Attempt1(vector<string> &options, string userInput){
+	// vector<string> possibleInputs;
+	int minPercentage = (int) (userInput.length() * 0.75);
+	priority_queue<pair<int, string>> minHeap;
+	// int longestStreak = 0;
+	// int streak = 0;
+
+	for(int i = 0; i < options.size(); i++){
+		minHeap.push(pair<int, string>(minDistance1(options[i], userInput), options[i]));
+	}
+
+    if(minHeap.top().second == userInput){
+        return {userInput};
+    }
+
+    //Only return the top 10 results
+    vector<string> closestMatch;
+    for(int i = 0; i < 10; i++){
+        closestMatch.push_back(minHeap.top().second);
+        minHeap.pop();
+    }
+	return closestMatch;
+}
+
+#endif // ATTEMPT_1
+
+/*vector<string> Attempt1(vector<string> &options, string userInput){
 	vector<string> possibleInputs;
 	int minPercentage = (int) (userInput.length() * 0.75);
 	int longestStreak = 0;
@@ -35,7 +89,7 @@ vector<string> Attempt1(vector<string> &options, string userInput){
 	}
 
 	return possibleInputs;
-}
+}*/
 
  /*
         int minPercentage = (int) (userInput.length() * 0.75);
--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include "Attempt1.hpp"
+#include "Attempt2.hpp"
 
-#include<bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 void printRecommendations(vector<string> recs){	
 	for(int i = 0; i < recs.size(); i++){
 		string items = recs[i] + "                                   ";
-		cout << items.substr(0, 40) << endl;
+		cout << items.substr(0, 40) << "\n";
 	}
+}
+
+void printTop10Recommendations(vector<string> recs){
+    int n = recs.size();
+    int numRuns = min(10, n);
+
+  	for(int i = 0; i < numRuns; i++){
+		string items = recs[i] + "                                   ";
+		cout << items.substr(0, 40) << "\n";
+	}  
 }
 
 vector<string> getMenu(string fileName){
@@ -28,7 +45,7 @@ vector<string> getMenu(string fileName){
 
 
 int main(int argv, char** argc){
-    string fileName = "words_alpha.txt";
+    string fileName = "words.txt";
 
     vector<string> options;
     options = getMenu(fileName);
@@ -39,19 +56,35 @@ int main(int argv, char** argc){
     getline(cin, userInput);
 
     //Attempt 1
+    cout << "\n";
     cout << "Running Attempt 1" << endl;
+    cout << "------------\n";
     auto start = std::chrono::high_resolution_clock::now();
     recs = Attempt1(options, userInput);
     auto stop = std::chrono::high_resolution_clock::now();
 
     cout << "Number of recommended words: " << recs.size() << endl;
-    // cout << "Sorry, we don't have " + userInput + ", do you mean:" << endl;
-    // printRecommendations(recs);
+    cout << "Top Word: " << recs[0] << endl;
+    cout << "Top Words: \n"; 
+    printTop10Recommendations(recs);
 
     cout << "Time to run: " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " milliseconds" << endl;
 
     //DP Solution
-    
+    //Attempt 1
+    cout << "\n";
+    cout << "Running Attempt 2 (DP)" << endl;
+    cout << "------------\n";
+    start = std::chrono::high_resolution_clock::now();
+    recs = Attempt2(options, userInput);
+    stop = std::chrono::high_resolution_clock::now();
+
+    cout << "Number of recommended words: " << recs.size() << endl;
+    cout << "Top Word: " << recs[0] << endl;
+    cout << "Top Words: \n"; 
+    printTop10Recommendations(recs);
+
+    cout << "Time to run: " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " milliseconds" << endl;
 
     return 0;
 }
